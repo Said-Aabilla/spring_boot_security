c2b03e993bc6069e9baf2c7ade75f1f6b43c4da8
Said-Aabilla <said160aabilla@gmail.com>
2021-12-31 15:28:00 +0100

    added user login

--- src/main/java/com/example/springsecurity/resource/UserResource.java
+++ src/main/java/com/example/springsecurity/resource/UserResource.java
@@ -1,18 +1,25 @@
 package com.example.springsecurity.resource;
 
+import com.example.springsecurity.constants.SecurityConstant;
+import com.example.springsecurity.domain.UserPrincipal;
 import com.example.springsecurity.exception.domain.EmailExistException;
 import com.example.springsecurity.exception.domain.ExceptionHandling;
 import com.example.springsecurity.exception.domain.UsernameExistException;
 import com.example.springsecurity.model.user.User;
 import com.example.springsecurity.service.UserService;
+import com.example.springsecurity.utility.JWTTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static com.example.springsecurity.constants.SecurityConstant.JWT_TOKEN_HEADER;
 import static org.springframework.http.HttpStatus.OK;
 
 @Controller
@@ -20,15 +27,39 @@ import static org.springframework.http.HttpStatus.OK;
 public class UserResource extends ExceptionHandling {
 
     private UserService userService;
+    //Auth helpers
+    private AuthenticationManager authenticationManager;
+    private JWTTokenProvider jwtTokenProvider;
 
     @Autowired
-    public UserResource(UserService userService) {
+    public UserResource(UserService userService, AuthenticationManager authenticationManager, JWTTokenProvider jwtTokenProvider) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
-    @PostMapping(value = "/register")
+    @PostMapping( "/register")
     public ResponseEntity<User> register(@RequestBody User user) throws EmailExistException, UsernameExistException {
        User newUser = userService.register(user.getFirstname(),user.getLastname(), user.getUsername(), user.getEmail());
        return new ResponseEntity<>(newUser, OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) {
+        authenticate(user.getUsername(), user.getPassword());
+        User loginUser = userService.findUserByUsername(user.getUsername());
+        UserPrincipal userPrincipal = new UserPrincipal(loginUser);
+        HttpHeaders jwtHeader = getJwtHeader(userPrincipal);
+        return new ResponseEntity<>(loginUser , jwtHeader,OK);
+    }
+
+    private void authenticate(String username, String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+    }
+
+    private HttpHeaders getJwtHeader(UserPrincipal userPrincipal) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(JWT_TOKEN_HEADER, jwtTokenProvider.generateJwtToken(userPrincipal));
+        return  headers;
     }
 }
